1d09efcde2485ac7c49c34e2ffca6c6dc3873c9f
abelcoss <41801571+abelcoss@users.noreply.github.com>
2018-10-19 11:04:59 -0400

    Add files via upload

---
+++ contact-form-handler.php
@@ -0,0 +1,23 @@
+<?php
+$name = $_Post['name'];
+$name = $_Post['name'];
+$name = $_Post['name'];
+$message = $_Post['message'];
+
+$email_form = 'abelcoss@gmail.com';
+
+$email_subject = 'Requisito de Informacion Chile';
+
+$email_body = "User Name: $name.\n".
+               "User LastName: $lastname.\n".
+                "User Email: $visito_email.\n".
+                "User Number: $number.\n".
+                  "User Message: $message.\n".;
+
+$to = "abelcoss@gmail.com";
+$headers = "From: $email_from \r\n";
+$headers .= "Reply-To: $visitor_email \r\n";
+mail($to,$email_subject,$email_body,$headers);
+header("Location: index.html");
+
+?>
